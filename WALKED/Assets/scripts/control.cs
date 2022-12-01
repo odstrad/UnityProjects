39670c3af39114404fb0d705b6afac499237624f
odstrad <dlvla827@learner.dvusd.org>
2022-12-01 09:15:48 -0700

    czxxczcxzczx

--- WALKED/Assets/scripts/control.cs
+++ WALKED/Assets/scripts/control.cs
@@ -6,6 +6,9 @@ using UnityEngine.InputSystem;
 public class control : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementx;
+    private float movementy;
+    public float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,15 @@ public class control : MonoBehaviour
     {
         
     }
-    void onmove(Inputvalue movementvalue)
+    void OnMove(InputValue movementvalue)
     {
         Vector2 movementvector = movementvalue.Get<Vector2>();
+        movementx=movementvector.x;
+        movementy=movementvector.y;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementx, 0.0f, movementy);
+        rb.AddForce(movement * speed);
     }
 }
